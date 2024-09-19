8782346dd6c09bc2a49750c9cfd4bf6411ef48cf
shahd-ai <145659831+shahd-ai@users.noreply.github.com>
2024-09-19 22:44:02 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+public static void main(String[] args) {
+
+    Animal lion=new Animal("lions","aaa",6,true);
+    Zoo zzz =new Zoo("mmm","ariana",20);
+    zzz.displayZoo();
+    lion.displayAnimal();
+
+
+}
+}
